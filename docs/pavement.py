6bead15be98742b04b5d6ef14b9994eee0374ba3
Drew Whitehouse <Drew.Whitehouse@anu.edu.au>
2009-08-31 12:28:51 +1000

    moved the pavement file for uploading html docs into the doc dir

---
+++ docs/pavement.py
@@ -0,0 +1,20 @@
+#
+# Automate hot build and distribution.
+#
+from __future__ import with_statement
+import os,sys,platform
+from paver.easy import *
+from paver.setuputils import setup
+
+try:
+    from paver.ssh import scp
+    @task
+    def update_docs():
+        """makes the html docs and pushes them to the web site"""
+        with pushd(path('docs')):
+            sh('make html')
+        sh('scp -r docs/_build/html/* sf:public_html/houdini/ocean/docs')
+except ImportError:
+    pass
+
+
